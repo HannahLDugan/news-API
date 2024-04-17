f6480356127af3f42e0ab3a536f3b00a19b1f2bc
HannahLDugan <155827170+HannahLDugan@users.noreply.github.com>
2024-04-17 10:37:14 +0100

    responds with article object with the necessary properties

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -1,5 +1,5 @@
 
-const { selectTopics, selectEndpoints } = require("../models/models")
+const { selectTopics, selectEndpoints, selectArticlesId } = require("../models/models")
 
 exports.getTopics = (req, res, next) => {
 		selectTopics().then((topics) => {
@@ -11,12 +11,24 @@ exports.getTopics = (req, res, next) => {
 	};
 
 exports.getEndpoints = (req, res, next) => {
-	selectEndpoints().then((endpoints) => {
+	selectEndpoints()
+	.then((endpoints) => {
 		res.status(200).send({ endpoints });
 	})
 	.catch((err) => {
 		next(err);
 	});		
+};
+
+exports.getArticlesId = (req, res, next) => {
+	const { article_id } = req.params
+	selectArticlesId(article_id)
+	.then((article) => {
+		res.status(200).send({article});
+	})
+	.catch((err) => {
+		next(err);
+	});
 };
 
 
